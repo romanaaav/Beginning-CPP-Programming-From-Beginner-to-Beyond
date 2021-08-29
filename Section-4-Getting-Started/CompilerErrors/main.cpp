61823d4428d715760c4d071f20bd61a44db4e677
Roman <romanaaav@gmail.com>
2021-08-29 02:00:56 -0300

    Added 'What are Compiler Errors' Lesson.
    -Renamed 'Getting-Started' lesson project to see them in order inside
    CodeLite IDE.

---
+++ Section-4-Getting-Started/CompilerErrors/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    //std::cout <<"Hello testing... <<std::endl; //<-- Missing terminating character
+    //std::cout <<("Hello testing..."/ 255) <<std::endl; //<-- Invalid operands
+    //std::cout <<"Hello testing..." <<std::endl //<-- Expected ';' before 'return'
+    std::cout <<"Hello testing..." <<std::endl;
+    //return 'Goodbye'; //<-- character cobstant too long for its type (Actually it's wrong to return a String instead of Int!!!)
+    return 0;
+}
