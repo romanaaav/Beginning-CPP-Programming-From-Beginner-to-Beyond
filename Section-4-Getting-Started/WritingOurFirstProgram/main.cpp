cd9b1e1a47e47f5d3c0b5a1a087705ae27f2dc58
Roman <romanaaav@gmail.com>
2021-08-29 01:30:06 -0300

    Added 'Writing our first program' lesson.

---
+++ Section-4-Getting-Started/WritingOurFirstProgram/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int favorite_number;
+    std::cout << "What's your favorite number between 1 and 100:";
+    std::cin >> favorite_number;    
+    std::cout << "Nice! That's my favorite number too!" << std::endl;
+    
+    return 0;
+}
